56e74e28900a5f0a739bea3f69873f04939d52a1
Abdul Rahman <abdulrahman161004@gmail.com>
2025-10-19 21:31:18 +0530

    Added simple Python addition program that takes two inputs and prints their sum

---
+++ main.py
@@ -0,0 +1,7 @@
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")   
+# taking two inputs from the user
+sum  = int(number1) + int(number2)
+
+# displaying the sum to the user
+print("The sum of", number1, "and", number2, "is:", int(sum))
